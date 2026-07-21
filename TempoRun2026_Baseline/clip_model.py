12718338bb22c5d12044464aedae0cb6a9974c2e
Bamboohoccode <tretrinh2k7@gmail.com>
2026-07-21 16:10:04 +0700

    u

--- TempoRun2026_Baseline/clip_model.py
+++ TempoRun2026_Baseline/clip_model.py
@@ -60,34 +60,39 @@ class ClipModel(nn.Module):
         feats = []
         for i in range(0, len(pil_images), batch_size):
             img_batch = pil_images[i:i + batch_size]
-            batch = self.processor(image = img_batch, return_tensors = "pt")
+            if len(img_batch) == 0:
+                continue
+            batch = self.processor(images = img_batch, return_tensors = "pt")
             img_inputs = {
-                key: value for key,value in batch.items() if key in {"pixel_values","pixel_attention_mask","spatial_shapes"}
+                key: value.to(self.device) for key,value in batch.items() if key in {"pixel_values","pixel_attention_mask","spatial_shapes"}
             }
             img_inputs["pixel_values"] = img_inputs[
                 "pixel_values"
-            ].to(dtype=self.model_dtype).to(self.device) # Only pixel_values need to transform in fp16/fp32
+            ].to(dtype=self.model_dtype)# Only pixel_values need to transform in fp16/fp32
 
             with torch.inference_mode():
                 features = self.model.get_image_features(**img_inputs)
-                features.F.normalize(features,dim = -1)
+                features = F.normalize(features,dim = -1)
                 features = (features.float().cpu().numpy().astype(self.np_dtype))
                 feats.append(features)
-        return np.concatenate(feats,axis =0) if feats else np.zeros((0, self.dim),self.model_dtype)
+        return np.concatenate(feats,axis =0) if feats else np.zeros((0, self.dim),self.np_dtype)
 
     def encode_texts(self, texts: list[str], batch_size=256) -> np.ndarray:
         feats = []
         for i in range(0, len(texts), batch_size):
+
             text_batch = texts[i:i + batch_size]
+            if len(text_batch) == 0:
+                continue
             toks = self.processor(text = text_batch,
                                   padding = "max_length",
                                   truncation = True,
                                   max_length = self.max_text_length,
                                   return_tensors = "pt").to(self.device)
             with torch.inference_mode():
-                features = self.model.get_text_features(toks)
+                features = self.model.get_text_features(**toks)
                 features = F.normalize(features,dim = -1)
-                feats.append(features.float().cpu().numpy().astype(self.model_dtype))
+                feats.append(features.float().cpu().numpy().astype(self.np_dtype))
     
-        return np.concatenate(feats, 0) if feats else np.zeros((0, self.dim), (self.model_dtype))
+        return np.concatenate(feats, 0) if feats else np.zeros((0, self.dim), (self.np_dtype))
     
